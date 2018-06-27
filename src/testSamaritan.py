7ffdd51c56294a548d18fa4bed77a189940bd3bb
__SYN__ <aero.sayan@gmail.com>
2018-06-27 21:20:59 +0530

    Presentation, presentation & presentation is all... Signed-off-by: __SYN__ <aero.sayan@gmail.com>

--- src/testSamaritan.py
+++ src/testSamaritan.py
@@ -7,4 +7,4 @@ baseMeshLevel = 0
 
 #sam.cy_addGeometry('../geoms/mod_naca2412.dat')
 #sam.cy_addGeometry('../geoms/naca2412.dat')
-sam.cy_runMeshGenerator(maxMeshLevel,baseMeshLevel,"geomScramjetTest.txt")
+sam.cy_runMeshGenerator(maxMeshLevel,baseMeshLevel,"geomCascadeTest.txt")
--- src/visualizeMesh.py
+++ src/visualizeMesh.py
@@ -22,10 +22,8 @@ y4 = y1
 
 print(x1.size)
 
-plt.plot([x1,x1,x1+geom[:,3],x1+geom[:,3],x1],[y1,y1+geom[:,2],y1+geom[:,2],y1,y1],'ko-',markerSize=2);
+plt.plot([x1,x1,x1+geom[:,3],x1+geom[:,3],x1],[y1,y1+geom[:,2],y1+geom[:,2],y1,y1],'ro-',markerSize=2);
 plt.title("mesh");plt.grid()
-orgGeom1= np.loadtxt("../geoms/scramjet_body.dat",dtype=np.float64)
-plt.plot(orgGeom1[:,0],orgGeom1[:,1],'r-')
-orgGeom2= np.loadtxt("../geoms/scramjet_ramp.dat",dtype=np.float64)
-plt.plot(orgGeom2[:,0],orgGeom2[:,1],'r-')
+orgGeom1= np.loadtxt("../geoms/mod_naca2412.dat",dtype=np.float64)
+plt.plot(orgGeom1[:,0],orgGeom1[:,1],'bo-')
 plt.show()
